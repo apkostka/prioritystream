361f3ef7ce3f1d6ee0fda49a7bb2fc2a19de490c
Andrew Kostka <akostka@gunnjerkens.com>
2013-06-27 16:54:28 -0700

    heroku seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create! name: "Andrew", email: "akostka@gunnjerkens.com", color: "000000", admin: true
