8eee2c2abb746fcbfc770777fa2e93dbb3dbacf3
Andrew Kostka <akostka@gunnjerkens.com>
2013-06-27 15:37:12 -0700

    fixed readme issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-  	@readme = File.read("/Library/WebServer/Documents/prioritystream/README.rdoc")
+  	@readme = File.read("/README.rdoc")
   end
 end
