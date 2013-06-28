3169dd87d04bc3f51ffde0a6076bcbe6e5306ca6
Andrew Kostka <akostka@gunnjerkens.com>
2013-06-28 11:42:05 -0700

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create name: "Andrew", email: "apkostka@gmail.com", color: "#366355"
-User.first.update_attribute(:created_at, "2013-06-20 20:34:23")
-User.first.update_attribute(:updated_at, "2013-06-21 01:56:39")
-User.first.update_attribute(:password_digest, "$2a$10$Av.sTbTSFQz0oc8bY/KbveOVBXj81sbI3g6oezmBJ.gD...")
-User.first.update_attribute(:remember_token, "mHeoVLfqGbA0bSIpWPoe9A")
-User.first.update_attribute(:admin, true)
+user = User.new name: "Andrew", email: "apkostka@gmail.com", color: "#366355", password: "123456", password_confirmation: "123456"
+user.update_attribute(:admin, true)
+user.save
